977b0a39664d65559cd13e308d4f2ff54ff1f288
bguo927 <101298454+bguo927@users.noreply.github.com>
2022-05-23 11:54:37 -0700

    follow player

--- PhysSim/Assets/Scripts/FollowPlayer.cs
+++ PhysSim/Assets/Scripts/FollowPlayer.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+    public Transform ball;
     public Vector3 offset;
-
+    private bool hasBall = true;
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position + offset;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hasBall = false;
+        }
+        if (hasBall)
+        {
+            transform.position = player.position + offset;
+        }
+
     }
 }
--- PhysSim/Assets/Scripts/MovePlayer.cs
+++ PhysSim/Assets/Scripts/MovePlayer.cs
@@ -2,22 +2,38 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    [SerializeField] private LayerMask playerMask;
 
     public Rigidbody rb;
+    public Rigidbody feet;
     public float moveForce = 500f;
     public Vector3 Movement;
+    private bool onGround = false;
+
 
     // Update is called once per frame
     void Update()
     {
         //Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
         Movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         
     }
 
     void FixedUpdate()
     {
-        rb.transform.position += Movement * moveForce * Time.deltaTime;
+        if (Physics.OverlapSphere(feet.position, 0.01f, playerMask).Length == 0)
+        {
+            rb.transform.position += Movement * moveForce * Time.deltaTime;
+            
+            return;
+        }
+        else
+        {
+            Movement = new Vector3(0, 0, 0);
+            Debug.Log("no one colliding!");
+
+        }
     }
     
 }
